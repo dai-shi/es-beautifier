f8c022561d72955064687db88cfcc2f44e0cb542
daishi <daishi@axlight.com>
2016-11-04 07:43:44 +0900

    unit test

---
+++ tests/lib/rules/multiline-block-statements.js
@@ -0,0 +1,19 @@
+const rule = require('../../../lib/rules/multiline-block-statements.js');
+const RuleTester = require('eslint').RuleTester;
+
+const ruleTester = new RuleTester();
+
+ruleTester.run('multiline-block-statements', rule, {
+  valid: [
+    'function f(){\na();\nb();\n}',
+  ],
+  invalid: [
+    {
+      code: 'function f(){a();b();}',
+      errors: [
+        { message: 'statement in a block to be on a new line.', type: 'BlockStatement' },
+        { message: 'statement in a block to be on a new line.', type: 'BlockStatement' },
+      ],
+    },
+  ],
+});
