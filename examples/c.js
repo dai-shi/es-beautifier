7b4d949cd13fd500b8d04a150ff89cec44fe5cb5
daishi <daishi@axlight.com>
2016-11-07 00:24:10 +0900

    nested Object/Array beautifier

--- examples/c.js
+++ examples/c.js
@@ -1,7 +1,8 @@
 const x=[1,2,3,4,5];
 const y={a:1,b:2,c:3,d:4};
-const z=[{a:1},{b:2},{c:3}];
-const z=[{a:1,x:'aaaaaaaaaaaaaa'},{b:2,x:'aaaaaaaaaaaaaa'},{c:3,x:'aaaaaaaaaaaaaa'}];
+const z1=[{a:1},{b:2},{c:3}];
+const z2=[{a:1,x:'aaaaaaaaaaaaaa'},{b:2,x:'aaaaaaaaaaaaaa'},{c:3,x:'aaaaaaaaaaaaaa'}];
 const a1=['aaaaaaaaaaaaaaaaaaaaaaa','bbbbbbbbbbbbbbbbbbbbbb','ccccccccccccccccccccccc','dddddddddddddddddddddddddddd'];
-const a2=[[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5]];
+const a2=[[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5],[1,2,3,4,5]];
 const o1={a:{x:100,y:101,z:102},b:{x:100,y:101,z:102},c:{x:100,y:101,z:102},d:{x:100,y:101,z:102},e:{x:100,y:101,z:102},}
+const o2={a:[1,2,3,4,500000000000000000],b:[2,3,4,5,600000000000000000]}
