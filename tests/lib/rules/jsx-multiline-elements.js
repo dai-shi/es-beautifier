2f54ed36c21a67d2ca1979c8b3f9ebd35e18f38f
daishi <daishi@axlight.com>
2016-11-11 08:37:28 +0900

    add test for jsx-multiline-elements

---
+++ tests/lib/rules/jsx-multiline-elements.js
@@ -0,0 +1,27 @@
+const rule = require('../../../lib/rules/jsx-multiline-elements.js');
+const RuleTester = require('eslint').RuleTester;
+
+const ruleTester = new RuleTester();
+
+const parserOptions = {
+  ecmaVersion: 6,
+  ecmaFeatures: { jsx: true },
+};
+
+ruleTester.run('jsx-multiline-elements', rule, {
+  valid: [{
+    code: 'x=<div>aaa</div>',
+    parserOptions,
+  }, {
+    code: 'x=<div>\n  aaa\n</div>',
+    parserOptions,
+  }],
+  invalid: [{
+    code: 'x=<div><span>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</span></div>',
+    errors: [1, 'and finally the end'].map(() => ({
+      message: 'Element in JSX should be on a new line.',
+      type: 'JSXElement',
+    })),
+    parserOptions,
+  }],
+});
