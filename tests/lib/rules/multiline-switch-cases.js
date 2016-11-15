292f517d38e4e0cd59e75e18697ebf0c375fef6f
daishi <daishi@axlight.com>
2016-11-16 06:27:50 +0900

    add multiline swith cases rule

---
+++ tests/lib/rules/multiline-switch-cases.js
@@ -0,0 +1,23 @@
+const rule = require('../../../lib/rules/multiline-switch-cases.js');
+const RuleTester = require('eslint').RuleTester;
+
+const ruleTester = new RuleTester();
+
+const t1 = 'SwitchStatement';
+const t2 = 'SwitchCase';
+
+ruleTester.run('multiline-switch-cases', rule, {
+  valid: [
+    'switch(x){\ncase 1:\ny=1;\nbreak;\n}',
+    'switch(x){\ncase 1:y=1;break;\n}',
+  ],
+  invalid: [{
+    code: 'switch(x){case 1:y=1;z=1;break;}',
+    errors: [t1, t2, t2, t2, t1].map(type => ({
+      message: type === t1 ?
+        'Switch case in a switch statement must be on a new line.' :
+        'Statement in a switch case must be on a new line.',
+      type,
+    })),
+  }],
+});
