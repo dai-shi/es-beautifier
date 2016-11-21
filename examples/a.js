e6a17d412c46edabd3b0f962611e8533eb0bbe4e
daishi <daishi@axlight.com>
2016-11-21 09:20:14 +0900

    switch-caes in example

--- examples/a.js
+++ examples/a.js
@@ -1,1 +1,2 @@
 function f(){/* this is a comment. */const x=[1,2,3,4];const y={a:1,b:2,c:3};if(x[0]===y.a){return x;}return y;}
+function g(x){switch(x){case 1:return"j";case 2:return"s";default:return"";}}
