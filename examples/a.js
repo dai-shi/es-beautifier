e959399e339fd1090ed09841045108a390116c45
daishi <daishi@axlight.com>
2016-11-24 10:39:45 +0900

    fix: better switch one line beautifier

--- examples/a.js
+++ examples/a.js
@@ -1,2 +1,2 @@
-function f(){/* this is a comment. */const x=[1,2,3,4];const y={a:1,b:2,c:3};const z=[[1,2,3],[4,5,6],[7,8,9]];if(x[0]===y.a){return x;}return y;}
-function g(x){switch(x){case 1:return"j";case 2:return"s";default:return"";}}
+function f(){/* this is a comment. */const x=[1,2,3,4];const y={a:1,b:2,c:3};const z=[[1,2,3],[4,5,6],[7,8,9]];if(x[0]===y.a){return x;}return "none";}
+function g(x){switch(x){case 1:return'j';case 2:return's';default:return'';}}
