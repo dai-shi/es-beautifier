717af4dc26879cd5a24220af46031562f8b524e5
daishi <daishi@axlight.com>
2016-12-04 12:00:08 +0900

    fix atom package

--- contrib/atom/index.js
+++ contrib/atom/index.js
@@ -1,6 +1,8 @@
+/* global atom */
+
 const path = require('path');
 const execFileSync = require('child_process').execFileSync;
-const atom = require('atom');
+const CompositeDisposable = require('atom').CompositeDisposable;
 
 function execCLI(input) {
   return execFileSync(path.resolve(__dirname, 'node_modules/es-beautifier/lib/cli.js'), {
@@ -19,7 +21,7 @@ function beautify() {
 }
 
 exports.activate = function activate() {
-  this.subscriptions = new atom.CompositeDisposable();
+  this.subscriptions = new CompositeDisposable();
   this.subscriptions.add(atom.commands.add('atom-workspace', 'es-beautifier', beautify));
 };
 
